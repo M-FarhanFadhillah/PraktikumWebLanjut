41c54757151f989eb2c6d223dcd13c6a8137cca2
M. Farhan Fadhillah <fadhillah28.mff@gmail.com>
2022-09-13 21:39:58 +0700

    Submit Tugas Pertemuan III

--- app/Database/Seeds/Mahasiswa.php
+++ app/Database/Seeds/Mahasiswa.php
@@ -5,7 +5,7 @@ namespace App\Database\Seeds;
 use CodeIgniter\Database\Seeder;
 use CodeIgniter\I18n\Time;
 
-class Makasiswa extends Seeder
+class Mahasiswa extends Seeder
 {
     public function run()
     {
