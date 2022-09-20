9d926bacd372b10aa56379dcf65f0fdfcdfb3fa2
M. Farhan Fadhillah <fadhillah28.mff@gmail.com>
2022-09-20 19:00:27 +0700

    Submit Tugas Pertemuan IV

--- app/Views/mahasiswa/list.php
+++ app/Views/mahasiswa/list.php
@@ -1,5 +1,5 @@
 <div class ="p-4">
-    <a href="/create" type="button" class="btn btn-primary mb-3" >Tambah Data </a>
+    <a href="/create" type="button" class="btn btn-success mb-3">Tambah Data</a>
     <table class="table table-striped ">
     <thead>
         <tr>
@@ -7,7 +7,8 @@
         <th scope="col">NPM</th>
         <th scope="col">Nama</th>
         <th scope="col">Alamat</th>
-        <th scope="col">created_at</th>
+        <th scope="col">Created_At</th>
+        <th scope="col">Action</th>
         </tr>
     </thead>
     <tbody>
@@ -19,6 +20,15 @@
         <td><?= $mhs['nama'] ?></td>
         <td><?= $mhs['alamat'] ?></td>
         <td><?= $mhs['created_at'] ?></td>
+        <td>
+            <div class="d-flex">
+                <a class="btn btn-warning" href="/edit/<?= $mhs['id'] ?>">Edit</a>
+                <form action="/delete/<?= $mhs['id']?>" method="post">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </td>
         </tr>
         <?php $no++; endforeach;?>
     </tbody>
